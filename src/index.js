208e730f33b8c42db0440d7a467c0bfa1f08f050
AbrarShahriar <AbrarShahriar321@gmail.com>
2023-03-05 16:50:06 +0600

    bare minimum

--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
-// @ts-nocheck
 const express = require("express");
-const http = require("http");
-const socket = require("socket.io");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4001;
 
 const app = express();
 app.use(cors());
-const httpServer = http.createServer(app);
-const io = socket(httpServer);
+app.use(express.json());
 
 let ledState = false;
-const EVENTS = {
-  LED_STATE_CHANGE: "ledState",
-};
 
-io.on("connection", (socket) => {
-  console.log(`new connection: ${socket.id}`);
-
-  socket.on("disconnect", () => {
-    console.log(`disconnected: ${socket.id}`);
-  });
-
-  io.to(socket.id).emit(EVENTS.LED_STATE_CHANGE, ledState);
-
-  socket.on(EVENTS.LED_STATE_CHANGE, (value) => {
-    ledState = value;
-    socket.broadcast.emit(EVENTS.LED_STATE_CHANGE, value);
-    console.log(ledState);
-  });
+app.get("/", (req, res) => {
+  res.status(200).json({ ledState });
 });
 
-httpServer.listen(PORT, () => {
+app.get("/toggle", (req, res) => {
+  console.log(req.body);
+  ledState = req.body.ledState;
+
+  res.status(200).json({ ledState: req.body.ledState });
+});
+
+app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
